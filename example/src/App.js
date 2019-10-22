d09f0643c4b772cd1dc139adacfd1d35ace5f3a5
Tengku Syafiq <tengku.mohd.syafiq@gmail.com>
2019-10-22 09:28:47 +0800

    change lock and unlock code

--- example/src/App.js
+++ example/src/App.js
@@ -473,7 +473,7 @@ class App extends React.Component {
                         backgroundColor: "#22509d"
                       }}
                       textStyle={{ color: "#fff" }}
-                      onPress={() => this.write(id, "unlock\r\n")}
+                      onPress={() => this.write(id, "85\r\n")}
                     />
                     <Button
                       title="lock" //{connected ? "unlock" : "lock"}
@@ -481,7 +481,7 @@ class App extends React.Component {
                         backgroundColor: "#22509d"
                       }}
                       textStyle={{ color: "#fff" }}
-                      onPress={() => this.write(id, "lock\r\n")}
+                      onPress={() => this.write(id, "15\r\n")}
                     />
                     {/* <Button
                       title="Write packets"
